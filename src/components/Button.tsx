ce2196934ce2f97dd60674e15fd446908c603d33
Gosia Magdzik <melassa26@gmail.com>
2024-03-13 10:28:51 +0100

    + ButtonStyles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,16 @@
-import React from 'react'
+import { cva } from "class-variance-authority";
+
+const ButtonStyles = cva(["hover:bg-secondary-hover",
+"transition-colors"], {
+  variants: {
+    size: {
+      default: [],
+      icon: []
+    }
+  }
+})
+
+const classes = ButtonStyles({ size: "icon" })
 
 export const Button = () => {
   return (
