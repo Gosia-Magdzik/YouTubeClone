5f7acefa02931fd502c3bbf6066bc05b852d7d12
Gosia Magdzik <melassa26@gmail.com>
2024-03-17 11:12:34 +0100

    ...styling. Right side of header has been added and styled

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,26 +1,38 @@
 import logo from "../assets/logo.png";
-import { Ghost, Menu } from 'lucide-react';
+import {  Bell, Menu, Upload, User } from 'lucide-react';
 import { Button } from "../components/Button";
 
 export const PageHeader = () => {
   return (
-    <div className="flex gap-10 lg:gap-20 justify-between">
+    <div className="flex gap-10 lg:gap-20 justify-between pt-2 mb-6 mx-4">
       <div className="flex gap-4 items-center flex-shrink-0">
         <Button 
           variant="ghost"
           size="icon"
-          className="m-10"
+          className="m-1"
         >
           <Menu/>
         </Button>
-        <a href="/" className="flex flex-row h-6 ">
+        <a href="/" className="flex flex-row h-6">
           <img src={logo} />
           <p>YouTube</p>
         </a>
       </div>
       <div></div>
-      <div></div>
-      <div></div>
+      <div className="flex flex-shrink md:gap-2">
+        <Button 
+          size="icon" 
+          variant="ghost"
+        >
+          <Upload/>
+        </Button>
+        <Button size="icon" variant="ghost">
+          <Bell/>
+        </Button>
+        <Button size="icon" variant="ghost">
+          <User/>
+        </Button>
+      </div>
     </div>
   )
 }
