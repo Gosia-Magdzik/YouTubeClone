6f90bae06229f7f47b5dc9c136b2775c7396e3ff
Gosia Magdzik <melassa26@gmail.com>
2024-03-21 14:38:48 +0100

    page body styling + new Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 
-import { PageHeader } from './layouts/PageHeader'
+import { PageHeader } from './layouts/PageHeader';
+import  CategoryPills  from './components/CategoryPills';
 
 function App() {
   return (
     <div className="max-h-screen flex flex-col">
       <PageHeader/>
-      
+      <div className='grid grid-cols-[auto, 1fr] flex-grow-1 overflow-auto'>
+        <div>Sidebar</div>
+        <div className='sticky top-0 bg-white z-10 pb-4'>
+          < CategoryPills />
+        </div>
+      </div>
     </div>
   )
 }
 
 export default App
+
